75e59849ff7a44b4e82f5f46106859de17963b4f
Victor Neves <info@vitorneves.com>
2021-02-13 12:36:42 +0000

    Initial commit

---
+++ frontend/src/auth0-config.js
@@ -0,0 +1,7 @@
+const auth0Config = {
+  audience: 'https://victorneves.eu.auth0.com/api/v2/',
+  domain: 'victorneves.eu.auth0.com',
+  client_id: 'sJKmjBzhwu6X4fANdrIIn8UAWhginpnW',
+  cacheLocation: 'localstorage',
+}
+export default auth0Config
